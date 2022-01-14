238a1d507f0c94160e988b86270e3a0a166b250a
EC2 Default User <ec2-user@ip-172-31-5-140.ec2.internal>
2022-01-14 08:15:34 +0000

    [add]新規登録機能、indexページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,25 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
+
+    @books = Book.all
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/homes/top'
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
