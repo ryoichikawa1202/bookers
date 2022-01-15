d22f62d5b17d91b45afef1bdb4d98e0a7e28d41f
RyoIchikawa <95997394+ryoichikawa1202@users.noreply.github.com>
2022-01-15 07:35:29 +0000

    [add]フラッシュ機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,14 +10,34 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to '/homes/top'
+    if book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(book.id)
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(book.id)
+    end
+  end
+
+  def delete
+    book = Book.find(params[:id])
+    if book.destroy
+      flash[:notice] = 'Book was successfully destroyed'
+      redirect_to index_book_path
+    end
   end
 
   private
